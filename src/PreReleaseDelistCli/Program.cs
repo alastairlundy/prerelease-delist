c632bc28802d8af017a5c1d2362c46c79f1c6274
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-20 14:36:27 +0000

    implement program.cs

--- src/PreReleaseDelistCli/Program.cs
+++ src/PreReleaseDelistCli/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PreReleaseDelistCli;
+using PreReleaseDelistLib;
 
+ConsoleApp.ConsoleAppBuilder app = ConsoleApp
+    .Create()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient();
+        services.AddSingleton<IPackageVersionService, PackageVersionService>();
+        services.AddSingleton<IPackageDelistService, PackageDelistService>();
+
+        IConfigurationBuilder configurationBuilder;
+
+        // Fallback to avoid using AppSettings.Json if it is not present.
+        try
+        {
+            configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables("PreReleaseDelistCLI_");
+        }
+        catch
+        {
+            configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables("PreReleaseDelistCLI_");
+        }
+        
+        IConfiguration configuration = configurationBuilder.Build();
+        
+        services.AddSingleton(configuration);
+    });
+
+app.Add<DelistCommand>();
+
+await app.RunAsync(args);
