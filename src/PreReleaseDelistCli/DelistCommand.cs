9f8ebbdae4f196918b8df08969499d3c8a25bf7a
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-23 18:47:54 +0000

    add license notices

--- src/PreReleaseDelistCli/DelistCommand.cs
+++ src/PreReleaseDelistCli/DelistCommand.cs
@@ -1,4 +1,22 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿/*
+    prerelease-delist - Delist pre-release library versions from a Nuget Server
+    Copyright (C) 2026 Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+     any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using Microsoft.Extensions.Configuration;
 using NuGet.Versioning;
 
 namespace PreReleaseDelistCli;
