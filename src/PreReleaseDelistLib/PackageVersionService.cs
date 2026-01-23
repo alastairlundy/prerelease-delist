21e1de6b5cc9654a0f304ac3e25c2cf528dc82d1
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-23 19:24:00 +0000

    add copyright notices

--- src/PreReleaseDelistLib/PackageVersionService.cs
+++ src/PreReleaseDelistLib/PackageVersionService.cs
@@ -2,6 +2,24 @@
 using NuGet.Common;
 using NuGet.Protocol;
 using NuGet.Protocol.Core.Types;
+﻿/*
+    PreReleaseDelistLib
+    Copyright (C) 2026 Alastair Lundy
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+     any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 
 namespace PreReleaseDelistLib;
 
