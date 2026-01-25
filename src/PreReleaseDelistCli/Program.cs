7a149ded28072bc070f8f63b6f8bd92c95089b49
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-01-25 14:06:32 +0000

    remove CliInvoke dependency

--- src/PreReleaseDelistCli/Program.cs
+++ src/PreReleaseDelistCli/Program.cs
@@ -16,13 +16,11 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-using CliInvoke.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 Cli.Ext.ConfigureServices(services =>
 {
     services.AddHttpClient()
-        .AddCliInvoke()
         .AddSingleton<IPackageVersionService, PackageVersionService>()
         .AddSingleton<IPackageDelistService, PackageDelistService>();
 
