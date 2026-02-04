9ab657a52d80d3d268ca24d8bc1c51c3ec7aa92e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-04 20:17:43 +0000

    add model

---
+++ src/PreReleaseDelistLib/Models/PackageVersionListingInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace PreReleaseDelistLib.Models;
+
+public class PackageVersionListingInfo
+{
+    public NuGetVersion PackageVersion { get; set; }
+    
+    public bool IsListed { get; set; }
+    
+    public bool PackageVersionExists { get; set; }
+}
