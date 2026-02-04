edb81e37c8e0c9cc749535e5cceefca6cdba3f68
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-02-04 20:26:22 +0000

    Create NuGetServiceRegistrationModel.cs

---
+++ src/PreReleaseDelistLib/Models/NuGetServiceRegistrationModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PreReleaseDelistLib.Models;
+
+public class NuGetServiceRegistrationModel
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("comment")]
+    public string Comment { get; set; } = string.Empty;
+}
