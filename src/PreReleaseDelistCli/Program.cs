966cf03136c2eab2e77a89e2d9104b1700b8fc14
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2026-03-13 16:12:39 +0000

    update dependencies

--- src/PreReleaseDelistCli/Program.cs
+++ src/PreReleaseDelistCli/Program.cs
@@ -15,6 +15,9 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+#if UseNetSdkDelistBackend
+using CliInvoke.Extensions;
+#endif
 
 using Microsoft.Extensions.DependencyInjection;
 using PreReleaseDelistLib.Abstractions;
@@ -24,6 +27,7 @@ Cli.Ext.ConfigureServices(services =>
     services.AddHttpClient()
         .AddSingleton<IPackageVersionService, PackageVersionService>()
 #if UseNetSdkDelistBackend
+        .AddCliInvoke(ServiceLifetime.Singleton)
         .AddSingleton<IPackageDelistService, NetSdkPackageDelistService>();
 #else
         .AddSingleton<IPackageDelistService, PackageDelistService>();
